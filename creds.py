32c31b3efe3f9e733be1fbefcb6256235c4e434b
OldKingCone <11233163+oldkingcone@users.noreply.github.com>
2018-04-17 17:59:41 -0700

    Create creds.py

---
+++ creds.py
@@ -0,0 +1,6 @@
+# you will need to create an app at apps.twitter.com
+# in order to get these keys, but once you have the keys put them in here.
+consumer_key = ''
+consumer_secret = ''
+access_key = ''
+access_secret = ''
