84ee2a16296abcd7e834ce517c27041cb7ffdcd2
OldKingCone <11233163+oldkingcone@users.noreply.github.com>
2018-04-23 20:53:27 -0700

    Delete creds.py

--- creds.py
+++
@@ -1,6 +0,0 @@
-# you will need to create an app at apps.twitter.com
-# in order to get these keys, but once you have the keys put them in here.
-consumer_key = ''
-consumer_secret = ''
-access_key = ''
-access_secret = ''
